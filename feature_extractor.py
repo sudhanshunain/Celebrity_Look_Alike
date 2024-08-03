47c413cbbb88d967cb3fb12fd793f3107d9c7d32
sudhanshunain <sudhanshunain265254@gmail.com>
2024-08-03 20:02:22 +0530

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,9 +1,3 @@
-# !pip install mtcnn==0.1.0
-# !pip install tensorflow==2.3.1
-# !pip install keras==2.4.3
-# !pip install keras-vggface==0.6
-# !pip install keras_applications==1.0.8
-
 #import os
 #import pickle
 
